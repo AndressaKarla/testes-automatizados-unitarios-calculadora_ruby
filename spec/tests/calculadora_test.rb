02cd9feafc5925ee78ee7e885a5d6b4d40e2ace6
AndressaKarla <andressa.karla1@gmail.com>
2024-04-12 13:44:47 -0300

    Adicionando teste para validar a operação de substrair

--- spec/tests/calculadora_test.rb
+++ spec/tests/calculadora_test.rb
@@ -4,13 +4,21 @@ describe Calculadora do
   end
 
   context "Somar" do
-    before(:all) do
+    it "Validar soma de números positivos de unidade e de centena" do
       @resultado_obtido = calculadora.somar(@calculadora_fixture['numero_positivo']['unidade'], @calculadora_fixture['numero_positivo']['centena2']);
 
       @resultado_esperado = @calculadora_fixture['numero_positivo']['unidade'] + @calculadora_fixture['numero_positivo']['centena2'];
-    end
 
-    it "Validar soma de números positivos de unidade e de centena" do
+      expect(@resultado_obtido).to eq(@resultado_esperado)
+    end
+  end
+
+  context "Subtrair" do
+    it "Validar subtração de número positivo de centena e número negativo de unidade" do
+      @resultado_obtido = calculadora.subtrair(@calculadora_fixture['numero_positivo']['centena1'], @calculadora_fixture['numero_negativo']['unidade']);
+
+      @resultado_esperado = @calculadora_fixture['numero_positivo']['centena1'] - @calculadora_fixture['numero_negativo']['unidade'];
+
       expect(@resultado_obtido).to eq(@resultado_esperado)
     end
   end
